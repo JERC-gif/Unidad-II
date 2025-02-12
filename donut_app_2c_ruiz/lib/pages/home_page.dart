4cd632444c4418934787aafba5594da4a589c01f
JERC-gif <jorgeaguila440@gmail.com>
2025-02-12 13:29:33 -0600

    main text added

--- donut_app_2c_ruiz/lib/pages/home_page.dart
+++ donut_app_2c_ruiz/lib/pages/home_page.dart
@@ -19,10 +19,49 @@ class _HomePageState extends State<HomePage> {
         color: Colors.grey[800]
         ),
         //Icono Derecho
-        actions: [
-          Icon(Icons.person)
+        actions:const [
+          Padding(
+            padding: EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )
         ],
-      )
+      ),
+      body: Column(
+        children: [
+      //Texto principal
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+          child: Row(
+            children: [
+              Text("I wan to  ", 
+              style: TextStyle(
+                //Tipografia 
+                fontFamily: "ComicStrip" ,
+                //Tamaño de letra
+                fontSize: 34
+              )
+              ),
+              Text("Eat", 
+              style: TextStyle(
+                //Tipografia 
+                fontFamily: "ComicStrip" ,
+                //Tamaño de letra
+                fontSize: 32,
+                //Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado
+                decoration: TextDecoration.underline
+              ),)
+            ],
+          ),
+        ),
+
+
+      //Pestaña (TaBar)
+
+      //Contenido(Cart)
+
+        ],)
     );
   }
 }
