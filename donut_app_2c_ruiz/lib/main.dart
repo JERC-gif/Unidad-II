894890d1a361836b5d521fc36d4be449040647d1
JERC-gif <jorgeaguila440@gmail.com>
2025-02-21 13:42:42 -0600

    StardingTabBarView

--- donut_app_2c_ruiz/lib/main.dart
+++ donut_app_2c_ruiz/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Uber Eats 2',
       theme: ThemeData(
         tabBarTheme: const TabBarTheme(indicatorColor: Colors.pink),
         
