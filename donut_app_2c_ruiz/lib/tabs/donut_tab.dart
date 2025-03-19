116d212f6de1275ca92505421641ffdcad14ec7c
Jorge Emmanuel Ruiz Castilla <jorgeaguila440@gmail.com>
2025-03-19 13:26:47 -0600

    Adding new products

--- donut_app_2c_ruiz/lib/tabs/donut_tab.dart
+++ donut_app_2c_ruiz/lib/tabs/donut_tab.dart
@@ -2,32 +2,81 @@ import 'package:donut_app_2c_ruiz/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class DonutTab extends StatelessWidget {
-
-//Lista de donas 
+  //Lista de donas
   final List donutsOnSale = [
-   // [ donutFlavor, donutPrice, donutColor, imageName ]
-   ["Ice Cream", "Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
-   ["Strawberry", "Dunkin donuts", "45", Colors.red, "lib/images/strawberry_donut.png"],
-   ["Grape Ape", "Aurrerá", "84", Colors.purple, "lib/images/grape_donut.png"],
-   ["Choco", "Costco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-];
-
-    DonutTab({super.key});
+    //[donutFlavor, donutStore, donutPrice, donutColor, imageName]
+    [
+      "Ice Cream",
+      "Krispy Cream",
+      "36",
+      Colors.blue,
+      "lib/images/icecream_donut.png"
+    ],
+    [
+      "Strawberry",
+      "Dunkin Donuts",
+      "45",
+      Colors.red,
+      "lib/images/strawberry_donut.png"
+    ],
+    [
+      "Grape Ape",
+      "Krispy Cream",
+      "84",
+      Colors.purple,
+      "lib/images/grape_donut.png"
+    ],
+    [
+      "Choco",
+      "Dunkin Donuts",
+      "95",
+      Colors.brown,
+      "lib/images/chocolate_donut.png"
+    ],
+    [
+      "Ice Cream",
+      "Krispy Cream",
+      "36",
+      Colors.blue,
+      "lib/images/icecream_donut.png"
+    ],
+    [
+      "Strawberry",
+      "Dunkin Donuts",
+      "45",
+      Colors.red,
+      "lib/images/strawberry_donut.png"
+    ],
+    [
+      "Grape Ape",
+      "Krispy Cream",
+      "84",
+      Colors.purple,
+      "lib/images/grape_donut.png"
+    ],
+    [
+      "Choco",
+      "Dunkin Donuts",
+      "95",
+      Colors.brown,
+      "lib/images/chocolate_donut.png"
+    ],
+  ];
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Cuadricula
     return GridView.builder(
-      //Numero de elementos en la cuadricula
-      itemCount: donutsOnSale.length,
-      padding: const EdgeInsets.all(8.0),
-      //Organiza como distribuir
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        //Numero de columnas
-        crossAxisCount: 2,
-        //Relacion de aspecto
-        childAspectRatio: 1/1.4), 
-        itemBuilder: (context, index){
+        //Cuantos elementos tiene
+        itemCount: (donutsOnSale.length),
+        padding: const EdgeInsets.all(12),
+        //Encargado de organizar la cuadrícula
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //Determinar número de columnas
+            crossAxisCount: 2,
+            childAspectRatio: 1 / 1.5),
+        itemBuilder: (context, index) {
+          //Elemento individual de la cuadrícula
           return DonutTile(
             donutFlavor: donutsOnSale[index][0],
             donutMark: donutsOnSale[index][1],
--- donut_app_2c_ruiz/lib/tabs/pizza_tab.dart
+++ donut_app_2c_ruiz/lib/tabs/pizza_tab.dart
@@ -1,12 +1,89 @@
+import 'package:donut_app_2c_ruiz/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class PizzaTab extends StatelessWidget {
-  const PizzaTab({super.key});
+  //Lista de donas
+  final List donutsOnSale = [
+    //[donutFlavor, donutStore, donutPrice, donutColor, imageName]
+    [
+      "Ice Cream",
+      "Krispy Cream",
+      "36",
+      Colors.blue,
+      "lib/images/icecream_donut.png"
+    ],
+    [
+      "Strawberry",
+      "Dunkin Donuts",
+      "45",
+      Colors.red,
+      "lib/images/strawberry_donut.png"
+    ],
+    [
+      "Grape Ape",
+      "Krispy Cream",
+      "84",
+      Colors.purple,
+      "lib/images/grape_donut.png"
+    ],
+    [
+      "Choco",
+      "Dunkin Donuts",
+      "95",
+      Colors.brown,
+      "lib/images/chocolate_donut.png"
+    ],
+    [
+      "Ice Cream",
+      "Krispy Cream",
+      "36",
+      Colors.blue,
+      "lib/images/icecream_donut.png"
+    ],
+    [
+      "Strawberry",
+      "Dunkin Donuts",
+      "45",
+      Colors.red,
+      "lib/images/strawberry_donut.png"
+    ],
+    [
+      "Grape Ape",
+      "Krispy Cream",
+      "84",
+      Colors.purple,
+      "lib/images/grape_donut.png"
+    ],
+    [
+      "Choco",
+      "Dunkin Donuts",
+      "95",
+      Colors.brown,
+      "lib/images/chocolate_donut.png"
+    ],
+  ];
+  PizzaTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("PizzaTab")
-    );
+    return GridView.builder(
+        //Cuantos elementos tiene
+        itemCount: (donutsOnSale.length),
+        padding: const EdgeInsets.all(12),
+        //Encargado de organizar la cuadrícula
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //Determinar número de columnas
+            crossAxisCount: 2,
+            childAspectRatio: 1 / 1.5),
+        itemBuilder: (context, index) {
+          //Elemento individual de la cuadrícula
+          return DonutTile(
+            donutFlavor: donutsOnSale[index][0],
+            donutMark: donutsOnSale[index][1],
+            donutPrice: donutsOnSale[index][2],
+            donutColor: donutsOnSale[index][3],
+            imageName: donutsOnSale[index][4],
+          );
+        });
   }
 }
